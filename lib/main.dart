4a9199d65d42859bc344786b6d22741a5c24bcb0
Asantoss <adrieldossantos7@gmail.com>
2022-01-22 13:58:25 -0300

    Adiciona importação de Widgets em main.dart e adiciona SingleChildScrollView().

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:expenses/models/transaction.dart';
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 main() => runApp(const ExtensesApp());
 
@@ -10,22 +9,14 @@ class ExtensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
         home: MyHome(),
     );
   }
 }
 
 class MyHome extends StatelessWidget {
-  final _transactions = [
-    Transaction(id: 't1', title: 'Novo tenis de Corrida', value: 310.76, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de luz', value: 210.76, date: DateTime.now()),
-  ];
-
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  MyHome({ Key? key }) : super(key: key);
+  const MyHome({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,93 +24,21 @@ class MyHome extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Despesas pessoais'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-           const SizedBox(
-              width: double.infinity,
-              child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
-            ),
-          ),
-          Column(
-            children: _transactions.map((tran){
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ), 
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        'R\$ ${tran.value.toStringAsFixed(2)}', 
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple
-                        ),
-                        )
-                      ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tran.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d/MMM/y').format(tran.date),
-                          style: TextStyle(
-                            color: Colors.grey.shade600
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: const InputDecoration(
-                      labelText: 'Titulo',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController, //formControlName
-                    decoration: const InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  TextButton(child: const Text('Nova transacao'), onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
-                  })
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: const [
+              SizedBox(
+                width: double.infinity,
+                child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
               ),
             ),
-          )
-        ],
+            TransactionUser()
+          ],
+        ),
       )
     );
   }
