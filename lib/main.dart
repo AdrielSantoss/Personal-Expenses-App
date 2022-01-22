df997793f8ac5ae0a046ccd0504c1795650b59b7
Asantoss <adrieldossantos7@gmail.com>
2022-01-22 14:52:24 -0300

    Adiciona refatoração em main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
-import 'package:expenses/components/transaction_user.dart';
+import 'dart:math';
+
+import 'package:expenses/components/transaction_form.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 main() => runApp(const ExtensesApp());
 
@@ -15,20 +20,55 @@ class ExtensesApp extends StatelessWidget {
   }
 }
 
-class MyHome extends StatelessWidget {
+class MyHome extends StatefulWidget {
   const MyHome({ Key? key }) : super(key: key);
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+
+  final _transactions = [
+    Transaction(id: 't1', title: 'Novo tenis de Corrida', value: 310.76, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Conta de luz', value: 210.76, date: DateTime.now()),
+  ];
+
+  _adicionarTransacion(String title, double value) => setState(() {
+      _transactions.add(
+        Transaction(
+          id: Random().nextDouble().toString(), 
+          title: title, value: value,
+          date: DateTime.now()
+        )
+      );
+    });  
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(adicionarTransacion: _adicionarTransacion);
+      } 
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Despesas pessoais'),
+        actions: [
+          IconButton(
+            onPressed: () => _openTransactionFormModal(context),
+            icon: const Icon(Icons.add))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-              SizedBox(
+          children: [
+              const SizedBox(
                 width: double.infinity,
                 child: Card(
                 color: Colors.blue,
@@ -36,10 +76,15 @@ class MyHome extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser()
+            TransactionList(transactions: _transactions)
           ],
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),  
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
