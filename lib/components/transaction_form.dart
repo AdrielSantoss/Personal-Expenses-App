5c5cb6033b55627c5b271873a241e145010df73c
Asantoss <adrieldossantos7@gmail.com>
2022-01-22 14:52:06 -0300

    Adiciona _submitForm() em transaction_form.dart

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -8,30 +8,43 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm({Key? key, required this.adicionarTransacion}) : super(key: key);
 
+  _submitForm(){
+    var title = titleController.text;
+    var value = double.tryParse(valueController.text) ?? 0;
+
+    if(title.isEmpty || value <= 0){
+        return;
+    }
+    adicionarTransacion(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: const InputDecoration(
-                      labelText: 'Titulo',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController, //formControlName
-                    decoration: const InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  TextButton(child: const Text('Nova transacao'), onPressed: () => adicionarTransacion(titleController.text, double.tryParse(valueController.text) ?? 0))
-                ],
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: const InputDecoration(
+                labelText: 'Titulo',
               ),
             ),
-        );
+            TextField(
+              controller: valueController, //formControlName
+              keyboardType: const TextInputType.numberWithOptions(decimal: true), //habilitar casas decimal no IOS
+              onSubmitted: (_) => _submitForm(),
+              decoration: const InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            TextButton(child: const Text('Nova transacao'), onPressed: () => _submitForm())
+          ],
+        ),
+      ),
+    );
   }
 }
